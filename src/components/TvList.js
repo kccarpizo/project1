d00bbbb872399ae23c64ff34d49ab12b77e15aea
kccarpizo <85187282+kccarpizo@users.noreply.github.com>
2021-08-14 21:45:30 -0400

    Update TvList.js

--- src/components/TvList.js
+++ src/components/TvList.js
@@ -1,13 +1,27 @@
-import {useContext} from 'react'
+import {useState, useEffect} from 'react'
 import TvListItem from './TvListItem'
 import '../assets/css/bootstrap.css';
 import movieContext from "../context/MovieContext";
 
 const TvList = () => {
-    const {tv,setTv} = useContext(movieContext);
+
+
+    const [movies2, setMovies2] = useState([]);
+
+        useEffect(() => {
+
+
+        fetch ("http://localhost:8080/tvs")
+        .then(res=>res.json())
+        .then(data => {console.log(data.bMessage); setMovies2(data.bMessage)})
+
+    }, [])
+    
+
+    
     return (
         <div className="movie-container">
-             {tv.map((tv)=>(<TvListItem key={tv.id} key={tv.id} title={tv.title} imgPath={tv.imgPath}/>))}
+             {movies2.map((tv)=>(<TvListItem key={tv.id} key={tv.id} title={tv.title} imgPath={tv.sm}/>))}
         </div>
     )
 }
